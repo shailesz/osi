d320eaff2da7117018ee39900a6fba0f54eb5a1c
shailesz <sailesh.karki@sagarmatha.edu.np>
2022-08-07 19:10:14 +0545

    init osi

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h1 className="blog-title">OSI</h1>
+        <h4 className="blog-title-author ms-2">by shailesz</h4>
+      </div>
+      <div className="mb-4">{Date()}</div>
+      <hr />
+      <div>
+        <h3 className="post-title">
+          <span className="me-2">#</span>well shit, and now i have a blog
+        </h3>
+        <div>
+          <p className="post-content">
+            <span className="post-date">Aug 07 -</span> Hello world! didn't mean
+            to start in a crappy way but it is what it is. i have never written
+            personal blogs and this is a fresh start. i believe i will keep this
+            blog going and turn writing into a habit. i have always been wanting
+            to start writing a blog and today i just found that little push i
+            needed. here's to a new hobby in the making 🥂
+          </p>
+          <p className="fst-italic">
+            what i feel today: some people come into your life and
+            bless you with inspiration
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
