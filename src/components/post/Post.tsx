2397bc0892da2cd7cf4ae2fddece04449e1fb577
shailesz <shailesz@duck.com>
2022-11-08 22:52:46 +0545

    update for multiple paragraphs

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Container, Title, Text, Group } from "@mantine/core";
 import { ContentfulPost } from "../../types/post";
 import {renderRichText} from 'gatsby-source-contentful/rich-text';
@@ -7,11 +7,22 @@ import { Options } from "@contentful/rich-text-react-renderer"
 import { prettyDate } from "../../helpers/time";
 
 const Post: React.FC<ContentfulPost> = ({ id, title, body, feelsLike, publishDate }) => {
+  let optionsIndex = 0;
+  
   const options: Options = {
     renderNode: {
-      [BLOCKS.PARAGRAPH]: (node, children) => <Text size="lg" weight={500} color="dark" span>{children}</Text>,
+      [BLOCKS.PARAGRAPH]: (node, children) => {
+        return <Text size="lg" weight={400} color="dark" my={16}>{children}</Text>},
     },
   }
+
+  const postBody: ReactNode[] = renderRichText(body, options) as ReactNode[];
+
+  postBody.map((node, index)=>{
+    
+
+    return node
+  })
   
   return (
     <Container mb={32}>
@@ -26,10 +37,12 @@ const Post: React.FC<ContentfulPost> = ({ id, title, body, feelsLike, publishDat
         </Text>
 
         <Text span>{title}</Text>
+
+       
       </Title>
       <Group>
         <Text size="lg" weight={500} color="dark">
-          <Text
+        <Text
             color="pink"
             variant="gradient"
             gradient={{ from: "#ed6ea0", to: "#ec8c69", deg: 35 }}
@@ -42,7 +55,8 @@ const Post: React.FC<ContentfulPost> = ({ id, title, body, feelsLike, publishDat
           <Text span mx={4}>
             -
           </Text>
-          {renderRichText(body, options)}
+          <Text size="lg" weight={400} color="dark" span>{postBody[0]?.props.children[0]}</Text>
+          {postBody.splice(1)}
         </Text>
         <Text italic color="dark">
           feels like: {feelsLike}
